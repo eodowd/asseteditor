1a977a7b268de5455d638122bd4e1d87b4048471
Eoin O'Dowd <46668191+eodowd@users.noreply.github.com>
2019-10-17 22:09:20 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template,  request
+from flask import Flask, render_template, request
 from pyfladesk import init_gui
 import os, sys
 import glob
 from helper import resource_path
+import base64
+from PIL import Image
+from io import BytesIO
+
+
+
 
 icon = resource_path(os.path.join('static', 'appicon.png'))
 if getattr(sys, 'frozen', False):
@@ -40,8 +46,29 @@ def import_files():
         return render_template('index.html',objects=objects)
 
 
+@app.route('/export', methods=['GET', 'POST'])
+def export_files():
+    objects = [os.path.basename(objects) for objects in glob.glob('static/*.glb')]
+    content = request.data
+    im = Image.open(BytesIO(base64.b64decode(content)))
+    im.save("temp/imag1e.png")
+    im.close()
+    return render_template('index.html',objects=objects)
+
+@app.route('/glb', methods=['GET', 'POST'])
+def glb_go():
+    objects = [os.path.basename(objects) for objects in glob.glob('static/*.glb')]
+    content = request.data
+    print(content)
+    file = open('temp/scene.glb', 'w')
+    file.write(content.decode("utf-8"))
+    file.close()
+
+    return render_template('index.html',objects=objects)
 init_gui(app,port=5000, width=1366, height=768,
              window_title="AssetEditor", icon="appicon.png", argv=None)
 
 if __name__ == '__main__':
     init_gui(app)
+
+
