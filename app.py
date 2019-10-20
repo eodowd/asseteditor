0785806cf92977ca7769ba4c504a513e4c9f5154
Eoin O'Dowd <46668191+eodowd@users.noreply.github.com>
2019-10-21 00:44:25 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ from io import BytesIO
 
 
 
-
 icon = resource_path(os.path.join('static', 'appicon.png'))
 if getattr(sys, 'frozen', False):
     template_folder = resource_path('templates')
     static_folder = resource_path('static')
-    temp_folder = resource_path('temp')
+
     app = Flask(__name__, template_folder=template_folder,
-                static_folder=static_folder,temp_folder=temp_folder)
+                static_folder=static_folder)
 else:
     app = Flask(__name__)
 
@@ -51,7 +50,7 @@ def export_files():
     objects = [os.path.basename(objects) for objects in glob.glob('static/*.glb')]
     content = request.data
     im = Image.open(BytesIO(base64.b64decode(content)))
-    im.save("temp/imag1e.png")
+    im.save("temp/image.png")
     im.close()
     return render_template('index.html',objects=objects)
 
@@ -59,12 +58,19 @@ def export_files():
 def glb_go():
     objects = [os.path.basename(objects) for objects in glob.glob('static/*.glb')]
     content = request.data
-    print(content)
-    file = open('temp/scene.glb', 'w')
+    newFileBytes = content
+    file = open("temp/scene.glb", 'wb')
+    file.write((''.join(chr(i) for i in newFileBytes)).encode('charmap'))
+    return render_template('index.html',objects=objects)
+@app.route('/collect', methods=['GET', 'POST'])
+def collect_go():
+    objects = [os.path.basename(objects) for objects in glob.glob('static/*.glb')]
+    content = request.data
+    save_type = content.decode("utf-8")
+    file = open('temp/scene.babylon', 'w')
     file.write(content.decode("utf-8"))
     file.close()
-
-    return render_template('index.html',objects=objects)
+    return render_template('index.html',objects=objects, save_type = save_type)
 init_gui(app,port=5000, width=1366, height=768,
              window_title="AssetEditor", icon="appicon.png", argv=None)
 
